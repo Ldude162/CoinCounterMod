86b28ce4b0dd74492e671a7a04bf323a4dc39fe0
TheZeldaBoi <63129353+Ldude162@users.noreply.github.com>
2021-02-03 19:33:30 -0800

    Changed version and modinfo

--- src/main/java/me/coincounter/ExampleMod.java
+++ src/main/java/me/coincounter/ExampleMod.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ExampleMod
 {
     public static final String MODID = "coincounter";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
